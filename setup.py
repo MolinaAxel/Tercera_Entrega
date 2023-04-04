6a9409c7b3badc666ee7e3970c92067c1936e102
Axel Molina <axelmaciel99@gmail.com>
2023-04-03 22:23:17 -0300

    tercera-entrega

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+
+    name="Axel_40425",
+    version="1.0",
+    description="",
+    author="Axel Molina",
+    author_email="axelmaciel99@gmail.com",
+    package=["tercera", "tercera_entrega"],
+
+
+)
